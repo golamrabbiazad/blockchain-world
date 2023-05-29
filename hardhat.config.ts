c7df21658674f1c88681b346dae4bb83aa4933f3
golamrabbiazad <golamrabbiazad@gmail.com>
2023-05-29 23:33:32 +0600

    chore: updated deps and re-structred ts-config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.18",
 };
 
 export default config;
